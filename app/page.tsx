afd987c1dfb2dd75a56668be06384ae54a50553e
cmeloni <carlo.meloni@abinsula.com>
2023-09-02 09:12:10 +0200

    feat: layout start

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@ import { UserButton } from '@clerk/nextjs'
 export default function Home() {
   return (
     <div>
-      <UserButton afterSignOutUrl="/"/>
+      <div className='grid grid-cols-1 md:grid-cols-3'>
+        <div className='bg-blue-100'>Booking</div>
+        <div className='col-span-2 bg-red-100 order-first md:order-last'>Map</div>
+      </div>
     </div>
   )
 }
